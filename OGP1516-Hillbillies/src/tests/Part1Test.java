285ae35ebf840ca4e8a778c3a4d92adf78f6b3de
Emiel <Emiel@192.168.0.107>
2016-03-06 14:21:17 +0100

    ...

--- OGP1516-Hillbillies/src/tests/Part1Test.java
+++ OGP1516-Hillbillies/src/tests/Part1Test.java
@@ -1,6 +1,8 @@
 package tests;
 
 import static org.junit.Assert.*;
+
+import org.junit.Before;
 import org.junit.Test;
 
 import hillbillies.model.Unit;
@@ -8,5 +10,10 @@ import hillbillies.model.Unit;
 public class Part1Test {
 
 	private Unit validUnit;
+	
+	@Before
+	public void setUpMutableFixture() {
+		Unit validUnit = new Unit("Harry", 50, 50, 50, 50, new int[] {5,5,5}, true);
+	}
 
 }
